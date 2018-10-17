9461dd3a53df04dfd54e32952de853bf48b16c6b
Walter Rafelsberger <walter@elastic.co>
2018-10-17 10:35:24 +0200

    Move file related code to its own fileReader module.

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import elasticsearch from 'elasticsearch';
 import glob from 'glob';
 
 import { createMappingFactory } from './_create-mapping';
+import { fileReaderFactory } from './_file-reader';
 import { indexQueueFactory } from './_index-queue';
 
 export function transformer({
@@ -26,71 +27,23 @@ export function transformer({
 
 	const createMapping = createMappingFactory({ client, targetIndexName, mappings, verbose });
 	const indexer = indexQueueFactory({ client, targetIndexName, typeName, bufferSize, skipHeader, verbose });
+	const indexFile = fileReaderFactory(indexer);
 
 	client.indices.exists({
 		index: targetIndexName
 	}, (err, resp) => {
 		if (resp === false) {
-			createMapping().then(indexFile);
+			createMapping().then(() => indexFile(fileName));
 		} else {
 			if (deleteIndex === true) {
 				client.indices.delete({
 					index: targetIndexName
 				}, (err, resp) => {
-					createMapping().then(indexFile);
+					createMapping().then(() => indexFile(fileName));
 				});
 			} else {
-				indexFile();
+				indexFile(fileName);
 			}
 		}
 	});
-
-	function indexFile() {
-		glob(fileName, function (er, files) {
-			startIndex(files);
-		});
-	}
-
-	function startIndex(files) {
-		const file = files.shift();
-		const s = fs.createReadStream(file)
-			.pipe(es.split())
-			.pipe(es.mapSync(function (line) {
-				s.pause();
-				try {
-					const doc = (typeof transform === 'function') ? transform(line) : line;
-					// if doc is undefined we'll skip indexing it
-					if (typeof doc === 'undefined') {
-						s.resume();
-						return;
-					}
-
-					// the transform callback may return an array of docs so we can emit
-					// multiple docs from a single line
-					if (Array.isArray(doc)) {
-						doc.forEach(d => indexer.add(d));
-						return;
-					}
-
-					indexer.add(doc);
-				} catch (e) {
-					console.log('error', e);
-				}
-			})
-			.on('error', function (err) {
-				console.log('Error while reading file.', err);
-			})
-			.on('end', function () {
-				verbose && console.log('Read entire file: ', file);
-				indexer.finish();
-				if (files.length > 0) {
-					startIndex(files);
-				}
-			})
-		);
-
-		indexer.queueEmitter.on('resume', () => {
-			s.resume();
-		});
-	}
 }
