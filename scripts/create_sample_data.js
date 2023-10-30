fa92520b4cd9a11e253d81debe103be2e5acf1bc
Walter Rafelsberger <walter.rafelsberger@elastic.co>
2023-10-30 13:49:58 +0100

    test: integration test for file-reader

---
+++ scripts/create_sample_data.js
@@ -0,0 +1,18 @@
+const fs = require('fs');
+
+const stream = fs.createWriteStream('./data/sample_data.ndjson', { flags: 'a' });
+
+const randomItemFromArray = items => items[~~(items.length * Math.random())];
+
+const codes = [200, 404, 500];
+const urls = ['login.php', 'post.php', 'home.php'];
+
+[...Array(10000)].forEach((item, index) => {
+  stream.write(
+    `${JSON.stringify({
+      the_index: index,
+      code: randomItemFromArray(codes),
+      url: randomItemFromArray(urls),
+    })}\n`,
+  );
+});
